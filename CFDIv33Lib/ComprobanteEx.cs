7a4d814094f37d3d5cf74a1f28f16987e8785012
Omar López <darkoware@gmail.com>
2017-10-24 12:49:05 -0500

    Primer commit

---
+++ CFDIv33Lib/ComprobanteEx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CFDIv33Lib
+{
+    public partial class Comprobante
+    {
+        public Comprobante()
+        {
+            //procesamiento de impuestos
+            this.versionField = "3.3";
+            Conceptos = new List<ComprobanteConcepto>();
+        }
+
+        public void GenerarXml(String rutaSalida)
+        {
+            EscribirXml(rutaSalida);
+        }
+
+        private void EscribirXml(String rutaSalida)
+        {
+            decimal total = 0;
+            foreach(ComprobanteConcepto concepto in conceptosField)
+            {
+                total += concepto.Importe;
+            }
+            this.totalField = total;
+            this.SubTotal = total;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Comprobante));
+            XmlTextWriter writer = new XmlTextWriter(rutaSalida, Encoding.UTF8);
+            writer.Formatting = Formatting.Indented;
+            writer.Indentation = 4;
+            serializer.Serialize(writer, this);
+            writer.Close();
+        }
+    }
+}
